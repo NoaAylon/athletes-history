08315eca185c7e6b67a5b171fcc4a1f496ee36de
noaEylon <37585299+NoaAylon@users.noreply.github.com>
2018-12-29 14:30:43 +0200

    redircting to posman link

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const athlete_ctrl = require('./controller/controller');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000
@@ -11,5 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 app.post('/athlete', athlete_ctrl.updateAthlete);
 app.get('/athletes', athlete_ctrl.getAllAthletes);
 app.get('/athlete/:winnings/:playSince', athlete_ctrl.getAthleteByWinningsAndPlaySince);
+
+app.get('*', (req, res) => {
+    res.redirect('https://documenter.getpostman.com/view/5655628/RznBMfHU')
+});
 
 app.listen(port, () => console.log(`Express server ready for request on: ${port}!`));
